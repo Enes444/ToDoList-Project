eff6fe703d803e7cb7f6b57c7fa8e9c62b61fb16
Enes Kulanoglu <eneskulanoglu@gmail.com>
2023-10-22 15:35:12 +0200

    init

---
+++ src/main/java/de/htwberlin/webtechnologien2/webtechnologien2/Webtechnologien2Application.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtechnologien2.webtechnologien2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webtechnologien2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webtechnologien2Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtechnologien2/webtechnologien2/Webtechnologien2ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtechnologien2.webtechnologien2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webtechnologien2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
